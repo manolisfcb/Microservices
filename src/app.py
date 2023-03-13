1288426fecb147fb1892570d62a378bbdb0674f4
manuel.alejandro <manuel.alejandro@oliveiratrust.com.br>
2023-03-13 14:52:15 -0300

    comentando las funciones

--- src/app.py
+++ src/app.py
@@ -1,26 +1,44 @@
 from flask import Flask, jsonify, render_template
 app = Flask(__name__)
 
-"""
-Creando un endpoint simples para la API
-http://127.0.0.1:5000
 
-Returns:
-    string: Hola mundo
-"""
 
 @app.route("/")
 def hello_word():
+    """
+    Creando un endpoint simples para la API
+    http://127.0.0.1:5000
+
+    Returns:
+        string: Hola mundo
+    """
     return "Hello, World!"
     
 
+
+
 @app.route("/health")
 def health_check():
+    """
+    Creando un endpoint simples para la API
+    http://127.0.0.1:5000/health
+
+    Returns:
+        json: {"status": "OK"}
+    """
+
     resp = jsonify({"status": "OK"})
     return resp
 
 @app.route("/details")
 def details():
+    """
+    Creando un endpoint simples para la API
+    http://127.0.0.1:5000/details
+
+    Returns:
+        trmplate html: index.html
+    """
     return render_template("index.html")
     
 
